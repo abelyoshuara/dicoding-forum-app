ace6ff7de2e7e83c565fec896c54ca51d0bad3b2
Abel Yoshuara <abelyoshuara@gmail.com>
2024-05-08 14:35:48 +0800

    make test pass

--- src/services/threadService.test.tsx
+++ src/services/threadService.test.tsx
@@ -30,6 +30,6 @@ describe("Thread service", () => {
       return result.current.isSuccess;
     });
 
-    expect(undefined).toEqual(mockData.getThreads.data.threads);
+    expect(result.current.data).toEqual(mockData.getThreads.data.threads);
   });
 });
