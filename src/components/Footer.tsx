cc8111b17e7d12dc3a5f91f511a49a2dde41d2fd
Abel Yoshuara <97333199+abelyoshuara@users.noreply.github.com>
2024-05-08 14:40:54 +0800

    make test passed (#2)
    * make test error
    
    * make test pass

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
     <div className="border py-5 dark:border-gray-800">
       <div className="container">
         <p className="text-center">
-          &copy; <NavLink to={"/"}>Dicoding Forum App</NavLink> &middot; 2024
+          &copy; <NavLink to={"/"}>Dicoding Forum</NavLink> &middot; 2024
         </p>
       </div>
     </div>
